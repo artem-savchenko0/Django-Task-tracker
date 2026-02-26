4054bb07ee6db31f0bee4da9518c3f2238b743e3
Artem Savchenko <256005153+artem-savchenko0@users.noreply.github.com>
2026-02-26 16:41:31 +0000

    add querysets

--- apps/task_tracker/querysets.py
+++ apps/task_tracker/querysets.py
@@ -2,3 +2,12 @@ from django.db import models
 
 
 class TaskQuerySet(models.QuerySet):
+	def for_user(self, user):
+		return self.filter(user=user)
+	
+	def todo(self):
+		return self.filter(status="todo")
+	
+
+	def completed(self):
+		return self.filter(status="done")
