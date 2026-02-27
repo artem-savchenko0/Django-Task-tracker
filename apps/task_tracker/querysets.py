53bc87d3c85501733c34b599616d66bfc566c34f
Artem Savchenko <256005153+artem-savchenko0@users.noreply.github.com>
2026-02-27 11:03:58 +0000

    add django auth to querysets

--- apps/task_tracker/querysets.py
+++ apps/task_tracker/querysets.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
+User = get_user_model()
 class TaskQuerySet(models.QuerySet):
-	def for_user(self, user):
+	def for_user(self, user: User):
 		return self.filter(user=user)
 	
 	def todo(self):
-		return self.filter(status="todo")
+		return self.filter(status=self.model.Status.TODO)
 	
-
 	def completed(self):
-		return self.filter(status="done")
+		return self.filter(status=self.model.Status.DONE)
