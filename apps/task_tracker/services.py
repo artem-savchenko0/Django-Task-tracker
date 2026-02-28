d065b1bf0c900599dbb8c7960caa26304be19f41
Artem Savchenko <256005153+artem-savchenko0@users.noreply.github.com>
2026-02-28 17:05:07 +0200

    Add def create_task to services.py

---
+++ apps/task_tracker/services.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from .models import Task
+
+User = get_user_model()
+
+def create_task(*, user: User, title: str, description: str = "", priority: int) -> Task:
+	task = Task(
+		user=user,
+		title=title,
+		description=description,
+		priority=priority,
+)
+	task.full_clean()
+	task.save()
+	return task 
