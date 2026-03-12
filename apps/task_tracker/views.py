72a9f208b025898ee3a012cef9bae80a1667bcf7
makhon23 <108093827+makhon23@users.noreply.github.com>
2026-03-12 21:49:48 +0200

    Fix TaskViewSet implementation

--- apps/task_tracker/views.py
+++ apps/task_tracker/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from .models.task import Task
+from .serializers import TaskSerializer
+
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer# Create your views here.
